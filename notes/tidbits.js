88136c53f0ca61040bb65937cdf8f3358da4c3cd
Jacob Cheney <jacob.cheney@sdl.usu.edu>
2022-06-24 20:20:48 +0000

    Add images, update bikes

--- notes/tidbits.js
+++ notes/tidbits.js
@@ -26,18 +26,6 @@ function display_gps(elt) {
     var gpx = e.target;
     map.fitBounds(gpx.getBounds());
     control.addOverlay(gpx, gpx.get_name());
-    
-    _t('h3').textContent = gpx.get_name();
-    _c('start').textContent = gpx.get_start_time().toDateString() + ', '
-      + gpx.get_start_time().toLocaleTimeString();
-    _c('distance').textContent = gpx.get_distance_imp().toFixed(2);
-    _c('duration').textContent = gpx.get_duration_string(gpx.get_moving_time());
-    _c('pace').textContent     = gpx.get_duration_string(gpx.get_moving_pace_imp(), true);
-    _c('elevation-gain').textContent = gpx.to_ft(gpx.get_elevation_gain()).toFixed(0);
-    _c('elevation-loss').textContent = gpx.to_ft(gpx.get_elevation_loss()).toFixed(0);
-    _c('elevation-net').textContent  = gpx.to_ft(gpx.get_elevation_gain()
-      - gpx.get_elevation_loss()).toFixed(0);
-      
   }).addTo(map);
 }
 
