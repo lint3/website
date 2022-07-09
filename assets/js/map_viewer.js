0a62db772750c4bffdb0b59b9aa4e0b1599ae50b
Jacob <jacob@cyptem.com>
2022-07-08 21:10:53 -0600

    Add layer selector

--- assets/js/map_viewer.js
+++ assets/js/map_viewer.js
@@ -8,17 +8,27 @@ function display_gps(elt) {
   function _t(t) { return elt.getElementsByTagName(t)[0]; }
   function _c(c) { return elt.getElementsByClassName(c)[0]; }
   
-  var map = L.map(mapid);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://www.osm.org">OpenStreetMap</a>'
-  }).addTo(map)
+
   
-  L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://www.osm.org">OpenStreetMap</a>'
+  });
+  
+  var otm = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  }).addTo(map)
-  ;
-  var control = L.control.layers(null, null).addTo(map);
+  });
+  
+  var map = L.map(mapid, {
+    layers: [osm]
+  );
+  
+  var maplayers = {
+    "OpenStreetMap": osm,
+    "OpenTopoMap": otm
+  };
+  
+  var layerControl = L.control.layers(osm, otm).addTo(map);
   
   new L.GPX(url, {
     async: true,
