46def86cea1fc9c9146a084d1906e83ed82ac491
Jacob <jacob@cyptem.com>
2022-07-30 16:46:59 -0600

    Add ability to build lanes and roads

--- assets/js/lanebuilder.js
+++ assets/js/lanebuilder.js
@@ -1,1 +1,24 @@
- 
+
+
+var buildLane = function(direction) {
+  var lane = document.createElement('div');
+  lane.setAttribute("class", "lane " + direction);
+  var label = document.createElement('p');
+  label.textContent = direction;
+  lane.appendChild(label)
+  return lane;
+}
+
+var buildRoad = function(tags) {
+  
+  var road = document.createElement('div');
+  road.setAttribute("class", "road");
+  road.appendChild(buildLane("forward"));
+  road.appendChild(buildLane("back"));
+  return road;
+  
+}
+
+var page = document.getElementsById("lanebuilder");
+var submitButton = page.getElementById("submittags");
+submitButton.setAttribute("onclick", "buildRoad()");
