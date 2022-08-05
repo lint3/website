210bfe243f13628958386be7e0cf98b14af236ab
Jacob Cheney <jacob.cheney@sdl.usu.edu>
2022-08-05 21:46:50 +0000

    Add dist alert

--- assets/js/leaflet-route.js
+++ assets/js/leaflet-route.js
@@ -4,15 +4,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-L.Routing.control({
+var routeControl = L.Routing.control({
     waypoints: [
         L.latLng(57.74, 11.94),
         L.latLng(57.6792, 11.949)
     ],
     routeWhileDragging: true
-}).addTo(map);
+});
 
-var routeControl = L.Routing.control({...});
+routeControl.addTo(map);
+
 
 routeControl.on('routesfound', function(e) {
    var routes = e.routes;
