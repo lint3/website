eb98a014e5f5ff43cb8698409692b942c4052205
Jacob Cheney <jacob.cheney@sdl.usu.edu>
2022-08-30 21:36:47 +0000

    This should do the trick

--- assets/js/park_hour.js
+++ assets/js/park_hour.js
@@ -3,7 +3,7 @@ var submitButton = document.getElementById("submit-button");
 submitButton.addEventListener('click', testInput);
 
 function testInput(parent) {
-  var bldgInput = document.getElementById("bldg").value;
+  var bldgInput = document.getElementById("building-input").value;
   var resultBox = document.createElement('div');
   resultBox.textContent = bldgInput;
   page.appendChild(resultBox);
