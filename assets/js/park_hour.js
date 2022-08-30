abd571064c432d32471e93bd6437133d2bb07a7b
Jacob Cheney <jacob.cheney@sdl.usu.edu>
2022-08-30 21:34:29 +0000

    This should do the trick

--- assets/js/park_hour.js
+++ assets/js/park_hour.js
@@ -1,3 +1,11 @@
 var page = document.getElementById("page");
 var submitButton = document.getElementById("submit-button");
-submitButton.addEventListener('click', buildRoad);
+submitButton.addEventListener('click', testInput);
+
+function testInput(parent) {
+  var bldgInput = document.getElementById("bldg").value;
+  var resultBox = document.createElement('div');
+  resultBox.textContent = bldgInput;
+  page.appendChild(resultBox);
+  
+}
