d46c2ed600a13f312db2b592918ddd24d7c5a33c
Jacob Cheney <jacob.cheney@sdl.usu.edu>
2022-09-02 22:42:54 +0000

    Add loading icon

--- assets/js/park_hour.js
+++ assets/js/park_hour.js
@@ -5,11 +5,21 @@ var bldgInput = document.getElementById("building-input")
 submitButton.addEventListener('click', testInput);
 
 function testInput(parent) {
-  var bldgInputResult = bldgInput.value;
-  resultSection.textContent = "Ride your bike to campus, then park it wherever you want for free!";  
+  // var bldgInputResult = bldgInput.value;
+  
+  var loadingIcon = document.createElement('img');
+  loadingIcon.setAttribute("id", "loading-icon");
+  loadingIcon.setAttribute("src", "/assets/icons/loading.gif");
+  resultSection.appendChild(loadingIcon);
+    
+  setTimeout(function() {
+    resultSection.removeChild(document.getElementById("loading-icon"));
+    resultSection.textContent = "Result: Ride your bike to campus, then park it wherever you want for free!";
+  }, 10);
+  
 }
 
-var map = L.map('map').setView([41.741, -111.808], 17);
+var map = L.map('map').setView([41.740, -111.807], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
