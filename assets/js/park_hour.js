116a79d43bfc7137447cf645521b4c79df939baf
Jacob Cheney <jacob.cheney@sdl.usu.edu>
2022-09-02 22:22:01 +0000

    Add parking calculator details

--- assets/js/park_hour.js
+++ assets/js/park_hour.js
@@ -1,12 +1,16 @@
 var page = document.getElementById("page");
+var resultSection = document.getElementById("results");
 var submitButton = document.getElementById("submit-button");
 var bldgInput = document.getElementById("building-input")
 submitButton.addEventListener('click', testInput);
 
 function testInput(parent) {
   var bldgInputResult = bldgInput.value;
-  var resultBox = document.createElement('div');
-  resultBox.textContent = bldgInputResult;
-  page.appendChild(resultBox);
-  
+  resultSection.textContent = "Ride your bike to campus, then park it wherever you want for free!";  
 }
+
+var map = L.map('map').setView([41.741, -111.808], 17);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
