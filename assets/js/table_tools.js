9ab14b96d111feb3134cd993aa01e9fe21694693
Jacob <git@cyptem.com>
2022-12-20 17:45:25 -0700

    Add table tools UI

--- assets/js/table_tools.js
+++ assets/js/table_tools.js
@@ -4,3 +4,43 @@ const table = document.querySelector('.ttools');
 var numCols = table.querySelector('thead tr').childElementCount;
 var numRows = table.querySelector('tbody').childElementCount;
 
+table.querySelectorAll('th')
+  .forEach((element, columnNo) => {
+    element.addEventListener('click', event => {
+      sortTable(table, columnNo);
+    });
+  });
+    
+
+element.prepend(generateColumnTools(element.getAttribute('datatype'));
+
+function setAttributes(element, attributes) {
+  for (attr in attributes) {
+    element.setAttribute(attr, attributes[attr]);
+  }
+}
+
+function generateColumnTools(type) {
+  var headerActions = document.createElement('div');
+  setAttributes(headerActions, {'class': 'header-actions'});
+  
+  var searchBox = document.createElement('input');
+  setAttributes(searchBox, {'type': 'text', 'class': 'column-search', 'placeholder': 'search'});
+  headerActions.appendChild(searchBox);
+  
+  var sortButton = document.createElement('input');
+  setAttributes(sortButton, {'type': 'button', 'value': 'sort', 'class': 'column-sort'});
+  headerActions.appendChild(sortButton);
+  
+  if (type == 'numeric') {
+    var minBox = document.createElement('input');
+    setAttributes(minBox, {'type': 'text', 'class': 'column-min', 'placeholder': 'min'});
+    headerActions.appendChild(minBox);
+    
+    var maxBox = document.createElement('input');
+    setAttributes(maxBox, {'type': 'text', 'class': 'column-max', 'placeholder': 'max'});
+    headerActions.appendChild(maxBox);
+  }
+  
+  return headerActions;
+}
