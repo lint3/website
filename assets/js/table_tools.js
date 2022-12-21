f7f9fb389ca5590a845d270142293a0dca45e5dd
Jacob <git@cyptem.com>
2022-12-21 19:05:12 +0000

    Add filter functionality

--- assets/js/table_tools.js
+++ assets/js/table_tools.js
@@ -9,7 +9,7 @@ const allTableData = table2data(tableBody);
 var actionData = {};
 
 var toolsButton = document.createElement('input');
-setAttributes(toolsButton, {'type': 'button', 'value': 'Filter, Sort, Etc.', 'class': 'tools-button'});
+setAttributes(toolsButton, {'type': 'button', 'value': 'filter/sort', 'class': 'tools-button'});
 toolsButton.addEventListener('click', (event) => { addTools(); });
 tableWrapper.appendChild(toolsButton);
 
@@ -30,12 +30,15 @@ function addTools() {
     actionData[i].dataType = headers[i].getAttribute('datatype');
   }
 
-
   // Add tools UI to each header cell
   table.querySelectorAll('th')
     .forEach((element, columnNo) => {
       element.appendChild(generateColumnTools(actionData[columnNo].dataType, columnNo));
     });
+    
+  var clearButton = document.createElement('input');
+  setAttributes(clearButton, {'class': 'clear-input', 'type': 'button', 'value': 'Clear All Filters'});
+  tableWrapper.appendChild(clearButton);
 }
 
 function applyActions() {
@@ -67,11 +70,13 @@ function setAttributes(element, attributes) {
 }
 
 function getColumnActions(col) {
-  actionData[col].query = headers[col].querySelector('.column-search').value;
   if(actionData[col].dataType == 'numeric') {
     actionData[col].min = headers[col].querySelector('.column-min').value;
     actionData[col].max = headers[col].querySelector('.column-max').value;
+  } else {
+    actionData[col].query = headers[col].querySelector('.column-search').value;
   }
+    
   return actionData[col];
 }
 
@@ -117,7 +122,12 @@ function generateColumnTools(type, columnNo) {
 }
 
 function filterTable(tableData, col, query) {
-  return tableData;
+  var result = [];
+  for (let i = 0; i < tableData.length; i++) {
+    if (tableData[i][col].includes(query)) {
+      result.push(tableData[i]);
+    }
+  }
 }
 
 function clipTable(tableData, col, min, max) {
@@ -215,8 +225,8 @@ function updateTable(tableData) {
     tableBody.appendChild(tr);
   });
   if (tableBody.innerHTML == "") {
-    noresults = document.createElement('div');
-    noresults.textContent = "No Results";
+    noResults = document.createElement('div');
+    noResults.textContent = "No Results";
     tableBody.appendChild(noResults);
   }
 }
